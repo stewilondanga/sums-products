5d112a011062b40673c80cc3faad7d7b778ee0ee
Stewart ilondanga <stilondanga@gmail.com>
2019-04-06 12:18:36 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -12,8 +12,8 @@ $("sub").each(function() {
     this.style.marginLeft = "-0.55em";
   }
 });
-/*$("sup").each(function() {
-    if (this.previousSibling.tagName == "SUB") {
-        this.style.margin = "0 -0.24em 0 -0.32em";
-    }
+$("sup").each(function() {
+  if (this.previousSibling.tagName == "SUB") {
+    this.style.margin = "0 -0.24em 0 -0.32em";
+  }
 });
